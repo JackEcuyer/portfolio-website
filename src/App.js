a2acf0546cb64d1200aecaa3213280a04d434195
Jack Ecuyer <58667720+JackEcuyer@users.noreply.github.com>
2022-11-04 22:41:05 +0000

    Installed tailwindcss
    Installed tailwindcss to CRA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Portfolio Website</div>;
+  return (
+    <div className="App">
+      <div className="container mx-auto text-xl text-center text-sky-500">
+        Portfolio Website
+      </div>
+    </div>
+  );
 }
 
 export default App;
